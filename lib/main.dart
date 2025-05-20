92256e0afb4053df4adef955e1cd0d634ab80fa1
eslammohammed <emohammed969@gmail.com>
2025-05-20 16:09:31 +0300

    Implement app-wide theming

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:quran_app/utils/colors_manager.dart';
 import 'package:quran_app/views/my_home_page.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 void main() async{
@@ -13,13 +14,41 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ScreenUtilInit(
-        designSize: const Size(392.72727272727275, 800.7272727272727),
-        builder: (context, child) =>  const MaterialApp(
-          debugShowCheckedModeBanner: false,
-          title: 'Flutter Quran',
-      
-          home: MyHomePage(),
-      )
+      designSize: const Size(392.73, 800.73),
+      builder: (context, child) => MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Flutter Quran',
+        theme: ThemeData(
+          fontFamily: 'Cairo',
+          scaffoldBackgroundColor: ColorManager.lilacPetals,
+          appBarTheme: const AppBarTheme(
+            backgroundColor: ColorManager.babyBlue,
+            elevation: 4,
+            centerTitle: true,
+            titleTextStyle: TextStyle(
+              color: ColorManager.deepBlue,
+              fontSize: 22,
+              fontWeight: FontWeight.bold,
+            ),
+            iconTheme: IconThemeData(color: ColorManager.deepBlue),
+          ),
+          textTheme: const TextTheme(
+            bodyLarge: TextStyle(
+              color: ColorManager.coldGrey,
+              fontSize: 16,
+            ),
+            bodyMedium: TextStyle(
+              color: ColorManager.deepBlue,
+              fontSize: 14,
+            ),
+          ),
+          colorScheme: ColorScheme.fromSwatch().copyWith(
+            primary: ColorManager.babyBlue,
+            secondary: ColorManager.deepBlue,
+          ),
+        ),
+        home: const MyHomePage(),
+      ),
     );
   }
 }
