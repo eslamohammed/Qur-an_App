8d669ee9510601bf61445c3efe951f90df2e8c6e
eslammohammed <emohammed969@gmail.com>
2025-05-20 12:17:27 +0300

    feat: Implement form container widget and color manager
    This commit introduces a reusable `FormContainerWidget` for text input fields and a `ColorManager` class for centralized color definitions.
    
    The `FormContainerWidget` provides a styled text field with options for:
    - Password visibility toggle
    - Prefix icons
    - Hint and label text
    - Validation and other standard `TextField` functionalities
    
    The `ColorManager` class defines various color constants to be used throughout the application, promoting consistency in the UI.

---
+++ lib/widgets/form_container.dart
@@ -0,0 +1,88 @@
+import 'package:flutter/material.dart';
+
+import '../utils/colors_manager.dart';
+
+class FormContainerWidget extends StatefulWidget {
+  final ValueChanged<String>? onChanged;
+
+  final TextEditingController? controller;
+  final Key? fieldKey;
+  final bool? isPasswordField;
+  final String? hintText;
+  final String? labelText;
+  final String? helperText;
+  final FormFieldSetter<String>? onSaved;
+  final FormFieldValidator<String>? validator;
+  final ValueChanged<String>? onFieldSubmitted;
+  final TextInputType? inputType;
+  final bool ? obscureText;
+  final IconData icon;
+
+
+
+  const FormContainerWidget(
+      {
+      Key? key, 
+      this.controller,
+      this.isPasswordField,
+      this.fieldKey,
+      this.hintText,
+      this.labelText,
+      this.helperText,
+      this.onSaved,
+      this.validator,
+      this.onFieldSubmitted,
+      this.inputType, this.obscureText, required this.icon, this.onChanged}) : super(key: key);
+
+  @override
+  // ignore: library_private_types_in_public_api
+  _FormContainerWidgetState createState() => _FormContainerWidgetState();
+}
+
+class _FormContainerWidgetState extends State<FormContainerWidget> {
+  bool _obscureText = true;
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      decoration: InputDecoration(
+        labelText: widget.labelText,
+        hintText: widget.hintText,
+          prefixIcon: widget.isPasswordField == true
+              ? const Icon(
+                  Icons.lock,
+                  color: Colors.black,
+                )
+              :
+                Icon( widget.icon,
+                  color: Colors.black,
+
+                ),
+          suffixIcon: GestureDetector(
+            onTap: () {
+              setState(() {
+                _obscureText = !_obscureText;
+              });
+            },
+            child: widget.isPasswordField == true
+                ? Icon(
+                    _obscureText ? Icons.visibility_off : Icons.visibility,
+                    color: _obscureText == false
+                        ? ColorManager.primary
+                        : ColorManager.secondary,
+                  )
+                : const Text(""),
+          ),
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(12),
+        ),
+      ),
+        style: Theme.of(context).inputDecorationTheme.hintStyle,
+        controller: widget.controller,
+        keyboardType: widget.inputType,
+        key: widget.fieldKey,
+        obscureText: widget.isPasswordField == true ? _obscureText : false,
+        onChanged: widget.onChanged,
+    );
+  }
+}
