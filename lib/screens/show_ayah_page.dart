998194ee59ff3a64b556413d699e2190cc27ec7d
eslammohammed <emohammed969@gmail.com>
2025-05-25 13:15:34 +0300

    feat: Add audio playback to read verse at ayah page

--- lib/screens/show_ayah_page.dart
+++ lib/screens/show_ayah_page.dart
@@ -1,8 +1,12 @@
+import 'package:audioplayers/audioplayers.dart';
 import 'package:easy_container/easy_container.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:quran/quran.dart';
+import 'package:quran_app/screens/quran_page.dart';
+import 'package:quran_app/widgets/Toast.dart';
 
 import '../services/network_handler.dart' as TafsirService;
 
@@ -43,6 +47,31 @@ class _ShowAyahState extends State<ShowAyah> {
         await TafsirService.fetchTafsir(widget.suraNumber, widget.ayahNumber);
     setState(() {}); // لتحديث الواجهة بعد جلب التفسير
   }
+
+    Future<void> play(String url) async {
+    await player.play(UrlSource(url));
+    print("SSSSSSSSSSSSSSSSSSSSSSS : $url");
+  }
+
+  int getGlobalVerseNumber(int surahNumber, int verseNumber) {
+  List<int> versesPerSurah = [
+    7, 286, 200, 176, 120, 165, 206, 75, 129, 109, // الفاتحة - يونس
+    123, 111, 43, 52, 99, 128, 111, 110, 98, 135,
+    112, 78, 118, 64, 77, 227, 93, 88, 69, 60,
+    34, 30, 73, 54, 45, 83, 182, 88, 75, 85,
+    54, 53, 89, 59, 37, 35, 38, 29, 18, 45,
+    60, 49, 62, 55, 78, 96, 29, 22, 24, 13,
+    14, 11, 11, 18, 12, 12, 30, 52, 52, 44,
+    28, 28, 20, 56, 40, 31, 50, 40, 46, 42,
+    29, 19, 36, 25, 22, 17, 19, 26, 30, 20,
+    15, 21, 11, 8, 8, 19, 5, 8, 8, 11,
+    11, 8, 3, 9, 5, 4, 7, 3, 6, 3,
+    5, 4, 5, 6
+  ];
+
+  int offset = versesPerSurah.sublist(0, surahNumber - 1).fold(0, (a, b) => a + b);
+  return offset + verseNumber;
+}
 
   @override
   void initState() {
@@ -102,8 +131,22 @@ class _ShowAyahState extends State<ShowAyah> {
                         mainAxisAlignment: MainAxisAlignment.end,
                         children: [
                           IconButton(
+                            onPressed: () {
+                              print("${getGlobalVerseNumber(widget.suraNumber, widget.ayahNumber)}");
+                              play(getAudioURLByVerseNumber(getGlobalVerseNumber(widget.suraNumber, widget.ayahNumber), reciter));
+                              if (kDebugMode) {
+                                  print("${getGlobalVerseNumber(widget.suraNumber, widget.ayahNumber)}");
+                                  print(getAudioURLByVerseNumber(getGlobalVerseNumber(widget.suraNumber, widget.ayahNumber), reciter));
+                                }
+                            },
+                            icon: const Icon(
+                              Icons.audiotrack_outlined,
+                              size: 24,
+                            )),
+                          IconButton(
                               onPressed: () {
                                 //setteng icon finctionality will be add later
+                                showToast("Comming soon",isError: false);
                               },
                               icon: const Icon(
                                 Icons.settings,
@@ -115,25 +158,13 @@ class _ShowAyahState extends State<ShowAyah> {
                   ],
                 ),
               ),
-              const Center(),
               Padding(
                 padding: const EdgeInsets.all(6.0),
                 child: EasyContainer(
                   color: Colors.green.withOpacity(0.05), ////
                   borderRadius: 12,
                   onTap: () async {
-                    // Navigator.push(
-                    //   context,
-                    //   MaterialPageRoute(
-                    //   builder: (builder) => QuranViewPage(
-                    //       shouldHighlightText: false,
-                    //       highlightVerse: "",
-                    //       jsonData: widget.suraJsonData,
-                    //       pageNumber: getPageNumber(ayatFiltered["result"][index]["surah"],ayatFiltered["result"][index]["verse"]),
-                    //       suraJsonData:widget.suraJsonData,
-                    //       ),
-                    //     ),
-                    //   );
+                    ////here
                   },
                   child: Text(
                     getVerse(widget.suraNumber, widget.ayahNumber,
