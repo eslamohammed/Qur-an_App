cdbf2e93ac1a4d92f565de9a6cca7da1da0cef93
eslammohammed <emohammed969@gmail.com>
2025-05-25 12:47:50 +0300

    Update Tafsir source to Jalalayn

--- lib/services/network_handler.dart
+++ lib/services/network_handler.dart
@@ -3,7 +3,7 @@ import 'dart:convert';
 
 
    Future<String> fetchTafsir(int surah, int ayah) async {
-      final url = Uri.parse('https://api.alquran.cloud/v1/ayah/$surah:$ayah/ar.ibn-kathir');
+      final url = Uri.parse('https://api.alquran.cloud/v1/ayah/$surah:$ayah/ar.jalalayn');
       final response = await http.get(url);
 
       if (response.statusCode == 200) {
